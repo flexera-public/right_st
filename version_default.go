97aa2f6d38505f22d0becb723409f4fff92d4aff
Douglas Thrift <douglas.thrift@rightscale.com>
2016-02-19 10:31:20 -0800

    Add version default file

---
+++ version_default.go
@@ -0,0 +1,7 @@
+// +build !make
+
+package main
+
+// vV is the default version string.
+// It gets overwritten by build scripts when building a release version.
+const VV = "right_st dev - unknown - commit"
